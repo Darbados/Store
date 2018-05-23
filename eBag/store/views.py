f19f03f8b8492327b443d63a0caee0ba2a6e7683
petar.n <petar.n@delasport.com>
2018-05-23 17:30:32 +0300

    adding some additional elements to the whole stuff

--- eBag/store/views.py
+++ eBag/store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, Product
 
 
 def index(request):
     template = 'store/index.html'
-    context = {'message': 'This is the home page!'}
+    categories = Category.objects.filter(tree_id=1)
+    context = {
+        'main_categories': categories,
+    }
     return render(request, template, context)
