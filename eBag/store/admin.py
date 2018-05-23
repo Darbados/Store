ffbd6a8308f281fe43b38b15c6469b852d6238c7
petar.n <petar.n@delasport.com>
2018-05-23 13:21:31 +0300

    keep it all in place

--- eBag/store/admin.py
+++ eBag/store/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
-from .models import Category
+from .models import Category, Product
 
 
 class CategoryAdmin(DjangoMpttAdmin):
-    pass
+    tree_auto_open = 0
+    list_display = ('name', )
+    ordering = ('name', )
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
 
 
+class ProductAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Product
+        fields = ('category', 'name', 'description', 'image', 'price')
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
--- eBag/store/models.py
+++ eBag/store/models.py
@@ -4,7 +4,29 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True)
-    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', db_index=True, on_delete=models.CASCADE)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}{1}".format(self.level, self.name)
 
     class MPTTMeta:
         order_insertion_by = ['name']
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, default="")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    image = models.ImageField(upload_to='products/{0}'.format(), blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    description = models.TextField(default="")
+    available = models.BooleanField(default=True)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{0} - {1}".format(self.name, self.description)
+
+    def get_category(self):
+        return self.category.name
+
