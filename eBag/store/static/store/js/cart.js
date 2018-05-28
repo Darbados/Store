c9ebef76689829d3aee5c49dc8bca5ce2f97db2a
petar.n <petar.n@delasport.com>
2018-05-28 16:53:48 +0300

    adding some additional elements to the whole stuff

--- eBag/store/static/store/js/cart.js
+++ eBag/store/static/store/js/cart.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 
     let initial_table_html = $(".table").html()
-    let products_info = check_storage()
-    cart_details(products_info, initial_table_html)
+    cart_details(initial_table_html)
 
     $(".cart_quantity_up").on('click', function(e){
         e.preventDefault()
@@ -41,45 +40,38 @@ $(document).ready(function(){
         }
 
         all_orders.splice(index, 1)
-        cart_details(all_orders, initial_table_html)
+        window.sessionStorage.setItem('Products', all_orders.join(','))
+        cart_details(initial_table_html)
     })
 });
 
 
-function cart_details(data, base){
-    let template;
+function cart_details(base){
+     if (window.sessionStorage.hasOwnProperty('Products') && window.sessionStorage.getItem('Products').length > 0){
+        let template;
+        all_products = window.sessionStorage.getItem('Products').split(',')
 
-    for (let x=0; x<data.length; x++){
-        if (data[x] != ''){
-            product_details = data[x].split('|')
-            product_id = product_details[0].replace(':','')
-            product_price = product_details[2].split('-')[1]
-            total_price = product_details[3].split('=')[1]
-            product_description = product_details[product_details.length-1].replace('?', ', ')
-            quantity = product_details[1].split('-')[1]
+        for (let x=0; x<all_products.length; x++){
+            if (all_products[x] != ''){
+                product_details = all_products[x].split('|')
+                product_id = product_details[0].replace(':','')
+                product_price = product_details[2].split('-')[1]
+                total_price = product_details[3].split('=')[1]
+                product_description = product_details[product_details.length-1].replace('?', ', ')
+                quantity = product_details[1].split('-')[1]
 
-            template += `<tbody><tr id=${product_id} data-price=${product_price} data-t_price=${total_price} data-quantity=${quantity}>
-                                <td class="cart_product"><a href=""><img src="{% static 'storeimages/cart/one.png' %}" alt=""></a></td>
-                                <td class="cart_description"><h4>${product_description}</h4><p>Web ID: 1089772</p></td>
-                                <td class="cart_price"><p>${product_price}lv.</p></td>
-                                <td class="cart_quantity"><div class="cart_quantity_button"><a class="cart_quantity_up" href="#"> + </a>
-                                <input class="cart_quantity_input" type="text" name="quantity" value=${quantity} autocomplete="off" size="2"><a class="cart_quantity_down" href="#"> - </a></div></td>
-                                <td class="cart_total"><p class="cart_total_price">${total_price}lv.</p></td>
-                                <td class="cart_delete"><a class="cart_quantity_delete" href="#"><i class="fa fa-times"></i></a></td></tr></tbody>`
+                template += `<tbody><tr id=${product_id} data-price=${product_price} data-t_price=${total_price} data-quantity=${quantity}>
+                                    <td class="cart_product"><a href=""><img src="{% static 'storeimages/cart/one.png' %}" alt=""></a></td>
+                                    <td class="cart_description"><h4>${product_description}</h4><p>Web ID: 1089772</p></td>
+                                    <td class="cart_price"><p>${product_price}lv.</p></td>
+                                    <td class="cart_quantity"><div class="cart_quantity_button"><a class="cart_quantity_up" href="#"> + </a>
+                                    <input class="cart_quantity_input" type="text" name="quantity" value=${quantity} autocomplete="off" size="2"><a class="cart_quantity_down" href="#"> - </a></div></td>
+                                    <td class="cart_total"><p class="cart_total_price">${total_price}lv.</p></td>
+                                    <td class="cart_delete"><a class="cart_quantity_delete" href="#"><i class="fa fa-times"></i></a></td></tr></tbody>`
 
+            }
         }
-    }
-    console.log(template)
-    $(".table").html(base + template)
-}
-
-
-function check_storage(){
-    let all_orders;
-
-    if (window.sessionStorage.hasOwnProperty('Products') && window.sessionStorage.getItem('Products').length > 0){
-        all_orders = window.sessionStorage.getItem('Products').split(',')
-    }
-
-    return all_orders;
+        console.log(template)
+        $(".table").html(base + template)
+     }
 }
